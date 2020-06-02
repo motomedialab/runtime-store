3405b0ffabcecc4b88721c73c6face0024c94d92
Chris Page <chris@motocom.co.uk>
2020-06-02 13:29:28 +0100

    Created HasRuntimeStore trait

---
+++ src/Traits/HasRuntimeStore.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author MotoMediaLab <hello@motomedialab.com>
+ * Created at: 02/06/2020
+ */
+
+namespace Motomedialab\RuntimeStore\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Motomedialab\RuntimeStore\RuntimeStore;
+
+/**
+ * Trait HasRuntimeStore
+ *
+ * @package Motomedialab\RuntimeStore\Traits
+ *
+ * @mixin Model
+ */
+trait HasRuntimeStore
+{
+    public function store(): RuntimeStore
+    {
+        return app(RuntimeStore::class)->group(static::class);
+    }
+}
